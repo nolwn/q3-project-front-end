0a18cc578ada9cbe484267da1a1c8f988541e18e
Christopher Kistner <chris.kistner0@gmail.com>
2019-01-15 15:02:21 -0800

    Search Form now working with Thunk

--- src/actions/cards.js
+++ src/actions/cards.js
@@ -3,26 +3,30 @@ export const SEARCH_CARDS = "SEARCH_CARDS";
 export const SHOW_MODAL = "SHOW_MODAL";
 export const HIDE_MODAL = "HIDE_MODAL";
 
-
-export const findCards = name =>
-  async dispatch => {
-    const response = await mtg.card.where({name, gameFormat:'standard'})
-    dispatch({
-      action: SEARCH_CARDS,
-      payload: response
-    })
-  };
+  export function findCards(name) {
+    return async (dispatch) => {
+        try {
+            const response = await mtg.card.where({name, gameFormat:'standard'})
+            dispatch({
+                type: SEARCH_CARDS,
+                payload: response
+            })
+        } catch (err) {
+            console.log(err)
+        }
+    }
+};
 
   export const showModal = () => {
     return {
         type: SHOW_MODAL,
         payload: true
       }
-  }
+  };
 
   export const hideModal = () => {
       return{
           type: HIDE_MODAL,
           payload: false
       }
-  }
+  };
