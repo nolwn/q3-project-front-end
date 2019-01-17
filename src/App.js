c8ac1a337e86bba9a272ea63f8201e5b203c108f
Nolan <nolan.hellyer@gmail.com>
2019-01-17 11:57:35 -0800

    fixed merge error

--- src/App.js
+++ src/App.js
@@ -2,16 +2,11 @@ import React, {Component} from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './App.css';
 import ListofDecks from './components/ListofDecks.js';
-<<<<<<< HEAD
 import LoginForm from './components/LoginForm';
 import AuthenticatedRoute from './higherOrderComponents/authenticatedRoute';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {verifyUser} from './actions/authentication';
-=======
-import DeckView from './components/DeckView'
-import { connect } from 'react-redux';
->>>>>>> working without redux yet
 
 class App extends Component {
   constructor(props) {
@@ -26,21 +21,17 @@ class App extends Component {
     return (
       <BrowserRouter>
       <div className='container'>
-<<<<<<< HEAD
           <div className="row" >
             <div className="col border bg-light">
             {/* <h2 className="navbar-brand bg-light">Search Magic Cards</h2>
               <SearchForm />
               <CardList foundCards = {this.props.cards} /> */}
               <Switch>
-                <AuthenticatedRoute path='/user_id/:user_id/decks' component={ListofDecks} /> 
+                <AuthenticatedRoute path='/user_id/:user_id/decks' component={ListofDecks} />
                 <Route path='/' component={LoginForm}/>
               </Switch>
               </div>
-          </div> 
-=======
-          <DeckView />
->>>>>>> working without redux yet
+          </div>
       </div>
     </BrowserRouter>
     )}
@@ -48,14 +39,9 @@ class App extends Component {
 
 const mapStateToProps = ({ cards }) => ({ cards });
 
-const mapDispatchToProps = dispatch => 
+const mapDispatchToProps = dispatch =>
 bindActionCreators({
   verifyUser,
 }, dispatch)
 
-<<<<<<< HEAD
 export default connect(mapStateToProps, mapDispatchToProps)(App);
-
-=======
-export default connect(mapStateToProps, null)(App)
->>>>>>> working without redux yet
--- src/store.js
+++ src/store.js
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger'
 import thunk from 'redux-thunk';
 import decks from './reducers/decks';
 import cards from './reducers/cards';
 import auth from './reducers/authentication';
-=======
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import logger from 'redux-logger'
->>>>>>> working without redux yet
 
 import decks from './reducers/decks'
 import cards from './reducers/cards'
@@ -17,11 +11,6 @@ import deckCards from './reducers/deckCards'
 
 
 //combineReducers
-<<<<<<< HEAD
-const reducers = combineReducers({auth, cards, decks})
-=======
-const reducers = combineReducers({cards, decks, deckCards})
->>>>>>> working without redux yet
+const reducers = combineReducers({auth, cards, decks, deckCards})
 
 export default createStore(reducers, applyMiddleware(thunk, logger))
-
