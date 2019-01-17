4da390ba7600e2decdddfc7aaa6c2f5d6a90ff89
Nolan <nolan.hellyer@gmail.com>
2019-01-16 16:14:42 -0800

    store quickfix

--- src/store.js
+++ src/store.js
@@ -2,11 +2,8 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import decks from './reducers/decks';
 import cards from './reducers/cards';
-<<<<<<< HEAD
-// import authentication from './reducers/authentication';
-=======
 import logger from 'redux-logger'
->>>>>>> add view deck page
+
 
 //combineReducers
 const reducers = combineReducers({cards, decks})
