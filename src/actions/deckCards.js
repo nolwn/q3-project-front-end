94beaecaf35204dd1959f41c09db2d60f4ae044f
Nolan <nolan.hellyer@gmail.com>
2019-01-17 20:31:44 -0800

    package-lock removed

--- src/actions/deckCards.js
+++ src/actions/deckCards.js
@@ -10,20 +10,25 @@ export const ADDDECKCARD = 'ADDDECKCARD'
 const server = process.env.REACT_APP_API_URL
 
 export const getDeckCards = (userId, deckId) => {
-    return async (dispatch) => {
-        const response = await axios.get(
-            server +
-            '/users/' + userId +
-            '/decks/' + deckId +
-            '/cards'
-        )
+    try {
+        return async (dispatch) => {
+            const response = await axios.get(
+                server +
+                '/users/' + userId +
+                '/decks/' + deckId +
+                '/cards'
+            )
 
-        dispatch({ type: DECKCARDS, payload: response.data })
+            dispatch({ type: DECKCARDS, payload: response.data })
+        }
+    } catch (err) {
+        console.log(err)
     }
 }
 
 export const decrement = (userId, deckId, cardId) => {
     return async (dispatch) => {
+        try {
             const response = await axios.patch(
                 server +
                 '/users/' + userId +
@@ -32,6 +37,10 @@ export const decrement = (userId, deckId, cardId) => {
                 '/remove'
             )
             dispatch({ type: DECREMENT, payload: response.data })
+
+        } catch (err) {
+            console.log(err)
+        }
     }
 };
 
@@ -47,6 +56,7 @@ export const increment = (userId, deckId, cardId) => {
                 '/add'
             )
             dispatch({ type: INCREMENT, payload: response.data })
+
         }catch(err) {
             console.log(err)
         }
@@ -54,29 +64,32 @@ export const increment = (userId, deckId, cardId) => {
 }
 
 export const addCard = (userId, deckId, cardData) => {
-    const body = convertCost(cardData.manaCost)
-    body.types = convertTypes(cardData.types, cardData.subtypes)
-    body.name = cardData.name
-    body.api_id = cardData.id
-
     return async (dispatch) => {
-        const postResponse = await axios.post(
-            server +
-            '/users/' + userId +
-            '/decks/' + deckId +
-            '/cards/add',
-            body
-        )
+        const body = convertCost(cardData.manaCost)
+        body.types = convertTypes(cardData.types, cardData.subtypes)
+        body.name = cardData.name
+        body.api_id = cardData.id
+        try {
+            const postResponse = await axios.post(
+                server +
+                '/users/' + userId +
+                '/decks/' + deckId +
+                '/cards/add',
+                body
+            )
 
-        const getResponse = await axios.get(
-            server +
-            '/users/' + userId +
-            '/decks/' + deckId +
-            '/cards'
-        )
+            const getResponse = await axios.get(
+                server +
+                '/users/' + userId +
+                '/decks/' + deckId +
+                '/cards'
+            )
 
-        dispatch({ type: ADDDECKCARD, payload: getResponse.data })
+            dispatch({ type: ADDDECKCARD, payload: getResponse.data })
 
+        } catch (err) {
+            console.log(err)
+        }
     }
 };
 
